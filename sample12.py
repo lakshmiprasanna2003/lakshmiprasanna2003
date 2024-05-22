9ef04acb8b6917dc0560c1370254a9b88fe10a7d
lakshmiprasanna2003 <lakshmiprasannamallam@gmail.com>
2024-05-22 10:54:43 +0530

    Add files via upload

---
+++ sample12.py
@@ -0,0 +1,15 @@
+import demoji
+
+demoji.download_codes()
+
+text = "I love Gaming 😊👍 #coding"
+text_with_descriptions = demoji.replace_with_desc(text, sep=":")
+
+print(text_with_descriptions)
+
+
+
+
+ First demoji ane oka module ni install cheyyali
+ After that import chesukovali
+ Using pip
